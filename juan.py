81a380e9075cf2fb29a97451f77e1a357eb703a2
Juan Felipe Rojas Muñoz <72052989+juanfero@users.noreply.github.com>
2023-07-12 12:03:16 -0500

    2 cambio header

--- juan.py
+++ juan.py
@@ -1,11 +1,12 @@
+pipe=int(input("edad: "))
+nicolle=int(input("edad: "))
+
+juanjo=12
+
 print("I am ready to learn")
 
 print("I have to learn everything, okkk")
 
 
-pipe=int(input("edad: "))
-nicolle=int(input("edad: "))
-
-juanjo=12
 
 
