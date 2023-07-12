f9894a991122d4f2b5be56a119e7399222882186
Juan Felipe Rojas Muñoz <juanferojas19@gmail.com>
2023-07-12 12:39:08 -0500

    changes

--- juan.py
+++ juan.py
@@ -1,11 +1,11 @@
-print("I am ready to learn")
+pipe=int(input("edad: "))
+nicolle=int(input("edad: "))
+juanjo=12
 
+print("I am ready to learn")
 print("I have to learn everything, okkk")
 
 
-pipe=int(input("edad: "))
-nicolle=int(input("edad: "))
 
-juanjo=12
 
 
